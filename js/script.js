795d16f08fd5225575e97b206d5023af60bbe6fb
palumbotomj <149309527+palumbotomj@users.noreply.github.com>
2024-03-14 11:16:30 -0400

    script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-document.getElementById('envelope-front').addEventListener('click', () => {
-    document.getElementById('envelope-front').classList.add('hidden');
+document.getElementById('envelope-front').addEventListener('click', function() {
+    this.classList.add('hidden');
     document.getElementById('envelope-back').classList.remove('hidden');
 });
 
-document.getElementById('envelope-back').addEventListener('click', () => {
-    document.getElementById('envelope-back').classList.add('hidden');
+document.getElementById('envelope-back').addEventListener('click', function() {
+    this.classList.add('hidden');
     document.getElementById('card-front').classList.remove('hidden');
 });
 
-document.getElementById('card-front').addEventListener('click', () => {
-    document.getElementById('card-front').classList.add('hidden');
+document.getElementById('card-front').addEventListener('click', function() {
+    this.classList.add('hidden');
     document.getElementById('card-inside').classList.remove('hidden');
 });
 
-document.getElementById('card-inside').addEventListener('click', () => {
-    document.getElementById('card-inside').classList.add('hidden');
+document.getElementById('card-inside').addEventListener('click', function() {
+    this.classList.add('hidden');
     document.getElementById('card-back').classList.remove('hidden');
 });
